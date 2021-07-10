34f8e9af46eb8eb9f687a0a60ffdc8de6055ecea
ssiwach <54826291+SSiwach@users.noreply.github.com>
2021-07-10 17:37:57 +0530

    Create using_variables.cpp

---
+++ using_variables.cpp
@@ -0,0 +1,14 @@
+// PROGRAM TO USE VARIABLE IN C++ PROGRAM
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  string character_name = "Green";
+  int age = 97;
+  cout<< "There is an old man named " << character_name<<"." <<endl;
+  cout<<"He was "<< age <<" years old."<<endl;
+  cout<<"He liked the name " << character_name<<"." <<endl;
+  cout<<"But he does not like being "<< age<< " years old." <<endl;
+return 0;
+}
