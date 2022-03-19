8a25400fec64c38a19bc559bf5e94314c4a88588
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-19 09:20:21 +0530

    Update gnuprb2.c

--- Msc_class/gnuprb2.c
+++ Msc_class/gnuprb2.c
@@ -1,20 +1,21 @@
+// program to calculate sinx and sin(nx + d) 
 #include<stdio.h>
 #include<math.h>
 #define pi 3.14159
 main()
 {
-float d,a,n,x,y,p;
-FILE *fp=NULL;
-fp=fopen("gnuprb2.txt","w");
-printf("enter d,a,n\n");
-scanf("%f,%f,%f",&d,&a,&n);
-d=d*pi;
-for(p=0;p<=2*pi;p+=0.01*pi)
-{
-x=sin(p);
-y=a*sin(n*p+d);
-printf("%f\t%f\n",x,y);
-fprintf(fp,"%f\t%f\n",x,y);
-}
+  float d,a,n,x,y,p;
+  FILE *fp=NULL;
+  fp=fopen("gnuprb2.txt","w");
+  printf("enter d,a,n\n");
+  scanf("%f,%f,%f",&d,&a,&n);
+  d=d*pi;
+  for(p=0;p<=2*pi;p+=0.01*pi)
+    {
+        x=sin(p);
+        y=a*sin(n*p+d);
+        printf("%f\t%f\n",x,y);
+        fprintf(fp,"%f\t%f\n",x,y);
+    }
 fclose(fp);
 }
