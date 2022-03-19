c68a48d922dff88be48d1b7f423a33cf97f44a4d
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-19 09:14:27 +0530

    Update gnuprb1a.c

--- Msc_class/gnuprb1a.c
+++ Msc_class/gnuprb1a.c
@@ -2,25 +2,26 @@
 #include<math.h>
 main()
 {
-float i,j;
-FILE *fp=NULL;
-fp=fopen("gnuprb1a.txt","w");
-j=0.0;
-for(i=0;i<=4;i+=0.1)
-{
-printf("%f \t%f \n",i,j);
- fprintf(fp,"%f \t %f\n",i,j);
-}
-i=4.0;
-for(j=0;j<=3;j+=0.1)
-{
-  printf("%f \t %f\n",i,j);
- fprintf(fp,"%f \t %f\n",i,j);
-}
-for(i=0;i<=4;i+=0.1)
-{j=((3/4)*i);
-  printf("%f \t %f\n",i,j);
-  fprintf(fp,"%f \t %f\n",i,j);
-}
-fclose(fp);
+ float i,j;
+ FILE *fp=NULL;
+ fp=fopen("gnuprb1a.txt","w");
+ j=0.0;
+  for(i=0;i<=4;i+=0.1)
+  {
+   printf("%f \t%f \n",i,j);
+   fprintf(fp,"%f \t %f\n",i,j);
+  }
+  i=4.0;
+  for(j=0;j<=3;j+=0.1)
+  {
+    printf("%f \t %f\n",i,j);
+    fprintf(fp,"%f \t %f\n",i,j);
+  }
+  for(i=0;i<=4;i+=0.1)
+  {
+    j=((3/4)*i);
+    printf("%f \t %f\n",i,j);
+    fprintf(fp,"%f \t %f\n",i,j);
+  }
+  fclose(fp);
 }
