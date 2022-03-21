29ad3add22fdce7254425f0be3d824fa5b2b81f3
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-21 08:07:03 +0530

    Update hcf.c

--- Msc_class/hcf.c
+++ Msc_class/hcf.c
@@ -1,17 +1,14 @@
+//program to find HCF of two numbers.
 #include <stdio.h>
 main()
 {
-int x,y,i,hcf;
-
-printf("enter two integer:n");
-scanf("%d,%d",&x,&y);
-
-for(i=1; i<=x && i<=y; ++i)
-{
-
-if(x%i==0 && y%i==0)
-hcf=i;
-}
-
-printf("HCF of %d and %d is %d" , x, y, hcf);
+    int x,y,i,hcf;
+    printf("enter two integer:n");
+    scanf("%d,%d",&x,&y);
+    for(i=1; i<=x && i<=y; ++i)
+    {
+       if(x%i==0 && y%i==0)
+       hcf=i;
+    }
+    printf("HCF of %d and %d is %d" , x, y, hcf);
 }
