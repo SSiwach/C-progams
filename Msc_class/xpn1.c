d89a2e79e020914a287392acbd61ea0b62f14fac
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-22 00:06:08 +0530

    Update xpn1.c

--- Msc_class/xpn1.c
+++ Msc_class/xpn1.c
@@ -2,25 +2,24 @@
 #include<math.h>
 float f(float x,float n)
 {
-  float y,z=1.0;
-int i;
-for (i=1;i<=n;i++)
-z*=x;
-if(x<=0.0)
-{
-y = z-1+exp(x);
-}
-else
-{
-y=z-log(1+x);
-}
+    float y,z=1.0;
+    int i;
+    for (i=1;i<=n;i++)
+      z*=x;
+      if(x<=0.0)
+      {
+         y = z-1+exp(x);
+      }
+      else
+      {
+         y=z-log(1+x);
+      }
 return(y);
 }
 main()
 {
   float x,n;
-printf("enter the value of x,(float),n(int)\n");
- scanf("%f , %f" , &x, &n);
-printf("x=%6.2f n=%6.2f f=%6.2f\n" ,x , n,f(x,n));
+  printf("enter the value of x,(float),n(int)\n");
+  scanf("%f , %f" , &x, &n);
+  printf("x=%6.2f n=%6.2f f=%6.2f\n" ,x , n,f(x,n));
 }
-
