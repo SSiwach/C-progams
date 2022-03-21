ab4077ef7477bbf8200eb4ae793d6ad524b57498
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-21 08:09:19 +0530

    Update mahi.c

--- Msc_class/mahi.c
+++ Msc_class/mahi.c
@@ -1,3 +1,4 @@
+//program to print value of x*x and x*x*x into a file using fp command
 #include<stdio.h>
 #include<math.h>
 main()
@@ -5,15 +6,13 @@ main()
   int i;
   float x,y;
 
-FILE *fp=NULL;
- fp=fopen("mahi.txt","w");
-for(i=1;i<100;i++)
-{x=i*i;
-  y= i*i*i;
-  fprintf(fp,"%d\t%f\t%f\n",i,x,y);
+  FILE *fp=NULL;
+  fp=fopen("mahi.txt","w");
+  for(i=1;i<100;i++)
+    {
+      x=i*i;
+      y= i*i*i;
+      fprintf(fp,"%d\t%f\t%f\n",i,x,y);
+    }
+    fclose(fp);
 }
-fclose(fp);
-}
-
-
-
