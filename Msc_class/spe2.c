e6a306c339e540dfc08eb46379196578e513eec5
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-23 07:50:11 +0530

    Update spe2.c

--- Msc_class/spe2.c
+++ Msc_class/spe2.c
@@ -2,17 +2,20 @@
 #include<math.h>
 main()
 {
-float x,t,s;
-int i,n;
- printf("enter x and n\n");
- scanf("%f,%d",&x,&n); 
-do
-{ 
- t=1.0;s=1;
- for(i=2;i<=n;i++)
-   {t*=(x/(i-1));s+=t;}
- printf("%f\t%f\t%d\n",s,t,n);
- }
+   float x,t,s;
+   int i,n;
+   printf("enter x and n\n");
+   scanf("%f,%d",&x,&n); 
+   do
+   { 
+     t=1.0;s=1;
+     for(i=2;i<=n;i++)
+     {
+       t*=(x/(i-1));
+       s+=t;
+     }
+     printf("%f\t%f\t%d\n",s,t,n);
+   }
  while (n>0);
  
 }
