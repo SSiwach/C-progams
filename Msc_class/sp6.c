b8e05ce6a8010a5c2d867e403908fd7aa428430c
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-23 07:53:17 +0530

    Update sp6.c

--- Msc_class/sp6.c
+++ Msc_class/sp6.c
@@ -2,20 +2,21 @@
 #include<math.h>
 main()
 {
-float t,s,z,a=0.01;
-int n;
-FILE *fp=NULL;
-fp=fopen("sp6.txt","w");
-for(z=-10;z<=1;z+=0.05)
-{
-t=1;s=1;n=1;
-do
-{
-  t*=(z*z*z)/((3*n)*(3*n-1));
-s+=t;
-n++;
- }while(fabs(t/s)>a);
- printf("%f\t%f\n",z,s*0.33503);
-fprintf(fp,"%f\t%f\n",z,s*0.35503);
- }
+  float t,s,z,a=0.01;
+  int n;
+  FILE *fp=NULL;
+  fp=fopen("sp6.txt","w");
+  for(z=-10;z<=1;z+=0.05)
+  {
+      t=1;s=1;n=1;
+  do
+    {
+        t*=(z*z*z)/((3*n)*(3*n-1));
+        s+=t;
+        n++;
+    }
+     while(fabs(t/s)>a);
+     printf("%f\t%f\n",z,s*0.33503);
+     fprintf(fp,"%f\t%f\n",z,s*0.35503);
+  }
 }
