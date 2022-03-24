99d9790731a07ffa38daa8a1c58553dc7de70373
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-24 08:50:44 +0530

    Update sinx.c

--- Msc_class/sinx.c
+++ Msc_class/sinx.c
@@ -2,10 +2,10 @@
 #include<math.h>
 main()
 {
-float x,y;
-printf("supply the value of x in radians\n");
-scanf("%f",&x);
-y = cos(x);
-printf("%f, %f\n", x,y);
+  float x,y;
+  printf("supply the value of x in radians\n");
+  scanf("%f",&x);
+  y = cos(x);
+  printf("%f, %f\n", x,y);
 }
 
