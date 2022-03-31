4abdea10940348918fd9fd2e3103669a91fd65db
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-03-31 08:48:47 +0530

    Update rpn3.c

--- Msc_class/rpn3.c
+++ Msc_class/rpn3.c
@@ -2,30 +2,30 @@
 #include<math.h>
 float f(float x,float y)
 {
-return(pow(x,3)+pow(y,3)+x*y+1);
+  return(pow(x,3)+pow(y,3)+x*y+1);
 }
 float g(float x,float y)
-{
-return(3*y*y+x);
-}
+  {
+    return(3*y*y+x);
+  }
 main()
 {
-FILE *fp=NULL;
-fp=fopen("rpn3.txt","w");
-float x=0,y,h,z;
-for(x=-1.5;x<=1.6;x+=0.1)
-{
-for(y=-2.0;y<=2.0;y+=1.85)
-{
-z=y;
-do
-{
-h=-f(x,z)/g(x,z);
-z=z+h;
-}
-while(fabs(h/z)>0.000001);
-printf("root=%f x=%f f(x,z)=%f\n",z,x,f(x,z));
-fprintf(fp,"%f\t%f\t%f\n",z,x,f(x,z));
-}
-}
+  FILE *fp=NULL;
+  fp=fopen("rpn3.txt","w");
+  float x=0,y,h,z;
+  for(x=-1.5;x<=1.6;x+=0.1)
+  {
+    for(y=-2.0;y<=2.0;y+=1.85)
+    {
+      z=y;
+      do
+      {
+        h=-f(x,z)/g(x,z);
+        z=z+h;
+      }
+      while(fabs(h/z)>0.000001);
+      printf("root=%f x=%f f(x,z)=%f\n",z,x,f(x,z));
+      fprintf(fp,"%f\t%f\t%f\n",z,x,f(x,z));
+    }
+  }
 }
