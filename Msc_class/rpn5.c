a872ff99583ff7372b0738679962ec55c43440bd
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-01 08:38:26 +0530

    Update rpn5.c

--- Msc_class/rpn5.c
+++ Msc_class/rpn5.c
@@ -2,31 +2,29 @@
 #include<math.h>
 float f(float x)
 {
-float a,b,c;
-int n;
-a=1.0;
-b=2.0*x;
-n=0;
-do
-{
-c=2.0*x*b - a ;
-a=b;
-b=c;
-n++;
+  float a,b,c;
+  int n;
+  a=1.0;
+  b=2.0*x;
+  n=0;
+  do
+  {
+    c=2.0*x*b - a ;
+    a=b;
+    b=c;
+    n++;
+  }
+  while(n<3);
+  return(c);
 }
-while(n<3);
-return(c);
-}
-
-
 main()
-{float x;
+{
+  float x;
   FILE *fp=NULL;
   fp=fopen("rpn5.txt","w");
   for(x=-1;x<=1.1;x+=0.1)
-    { printf("%f\t%f\n",x,f(x));
- fprintf(fp,"%f\t%f\n",x,f(x));
-
-
-
-
+    { 
+      printf("%f\t%f\n",x,f(x));
+      fprintf(fp,"%f\t%f\n",x,f(x));
+    }
+}
