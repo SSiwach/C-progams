95ff3ce4680b0b30de9d04b7a18bac462a4c007f
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-05 21:40:33 +0530

    Update mshi1.c

--- Msc_class/mshi1.c
+++ Msc_class/mshi1.c
@@ -2,13 +2,17 @@
 #include <math.h>
 main()
 {
-int i;
-flot x,y;
-FILE *fp=NULL;
-fp=fopen("num.txt","w");
-for(i=1;i<=1000;i++)
-{x=i*i}
-for(i=1;i<=1000;i++)
-{y=i*i*i;}
-fprintf(fp,"%d \t %f\t %f\n",i,x,y);
+  int i;
+  flot x,y;
+  FILE *fp=NULL;
+  fp=fopen("num.txt","w");
+  for(i=1;i<=1000;i++)
+  {
+    x=i*i
+  }
+  for(i=1;i<=1000;i++)
+  {
+    y=i*i*i;
+  }
+  fprintf(fp,"%d \t %f\t %f\n",i,x,y);
 }
