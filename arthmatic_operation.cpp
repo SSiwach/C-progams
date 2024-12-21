dcae86c6cd194582c41a67779f5e2dfd815fd35e
Surender <54826291+SSiwach@users.noreply.github.com>
2024-12-21 10:47:03 +0530

    Create arthmatic_operation.cpp

---
+++ arthmatic_operation.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 6;
+  int y = 3;
+
+  cout << x+y <<endl;
+  cout << x-y <<endl;
+  cout << x*y <<endl;
+  cout << x/y <<endl;
+
+
+}
