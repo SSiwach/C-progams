82749b182012540c8056ac230cd18d089e861fc2
Surender <54826291+SSiwach@users.noreply.github.com>
2024-12-21 10:26:44 +0530

    Create use_of_endl.cpp

---
+++ use_of_endl.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  std::cout << "Hello World!";
+  std::cout<<endl;
+  std::cout<< " This program is not for you! "<<endl; 
+  std::cout<<"This is not a test program"<<endl;
+  std::cout<<endl;
+
+}
